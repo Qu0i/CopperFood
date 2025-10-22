0ed916e071dd6edb0eac45aaf012828216cfe520
Qu0i <quoiteam.com@gmail.com>
2025-10-22 17:22:30 +0300

    [Init] Project created

---
+++ src/client/java/com/qu0i/copperfood/client/CopperfoodClient.java
@@ -0,0 +1,10 @@
+package com.qu0i.copperfood.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CopperfoodClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
---
+++ src/main/java/com/qu0i/copperfood/Copperfood.java
@@ -0,0 +1,17 @@
+package com.qu0i.copperfood;
+
+import com.qu0i.copperfood.items.ModItems;
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Copperfood implements ModInitializer {
+    public static final String MOD_ID = "copperfood";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+    @Override
+    public void onInitialize() {
+        LOGGER.info("Initialized.");
+        ModItems.initialize();
+    }
+}
